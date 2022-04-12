37a709e36c77b6d7b5daf35bdc8d1832b186d569
Max Sobkov <sobkovmax@gmail.com>
2022-04-12 15:48:43 -0400

    Storing twitter API key in secret.env file. YOU NEED TO CREATE YOUR OWN WITH YOUR OWN KEY SINCE THE GITIGNORE IS PROTECTING KEYS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config({ path: './secret.env' });
 
 const {getTwitterUserData} = require("./endpoints/twitter_endpoints");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
